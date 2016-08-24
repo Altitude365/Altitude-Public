ddd83fc98a9b3fa40caf9a0e068f6446aeb21012
jonjander <jon.jander@altitude365.com>
2016-08-24 08:42:14 +0200

    Added comments and cleanup

--- Tools/Azure RBAC Creator/Azure RBAC Creator.ps1
+++ Tools/Azure RBAC Creator/Azure RBAC Creator.ps1
@@ -1,11 +1,13 @@
-﻿Login-AzureRmAccount
+﻿#Login
+Login-AzureRmAccount
 
+#Select Subscription
 $sub = Get-AzureRmSubscription | ogv -OutputMode Single
 Select-AzureRmSubscription -SubscriptionName $sub.SubscriptionName
 
 function get-subOps {
 param ($RootOP)
-    [string[]]$subOps=(Get-AzureRmProviderOperation "$RootOP").Operation
+    [string[]]$subOps=(Get-AzureRmProviderOperation "$RootOP" | select Operation).Operation #Get Operation
     return $subOps
 }
 
@@ -14,20 +16,19 @@ param (
 [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()]$Role,
 [Parameter(Mandatory=$true)][ValidateSet("Actions","NotActions")]$type
 ) #add pipeline
-    [object[]]$ProviderOperations = Get-AzureRmProviderOperation *
-    [string[]]$Ops = $ProviderOperations.Operation
+    [object[]]$ProviderOperations = Get-AzureRmProviderOperation * #Get all Provider Operations
+    [string[]]$Ops = $ProviderOperations.Operation #Filter out the Operation Value
 
-    [string]$RootOP = $Ops.ForEach({($psitem -split "/")[0]}) | sort | Get-Unique | ogv -OutputMode Single
-    [string[]]$subLevel = get-subOps -RootOP "$RootOP/*"
+    [string]$RootOP = $Ops.ForEach({($psitem -split "/")[0]}) | sort | Get-Unique | ogv -OutputMode Single #Select top Operation
+    [string[]]$subLevel = get-subOps -RootOP "$RootOP/*" #Get all sublevel Operations
     [int]$cLvel=1
-    [int]$maxLevel = ($subLevel.ForEach({($psitem -split "/").count}) | measure -Maximum).Maximum
+    [int]$maxLevel = ($subLevel.ForEach({($psitem -split "/").count}) | measure -Maximum).Maximum #Get the maximum deph of the operation
     do
     {
         [bool]$End=$false
         [string[]]$subLevel = get-subOps -RootOP "$RootOP/*"
         [int]$maxLevel = ($subLevel.ForEach({($psitem -split "/").count}) | measure -Maximum).Maximum
-        [string[]]$Options = $subLevel.ForEach({($psitem -split "/")[$cLvel]}) | sort | Get-Unique
-        write-host $Options
+        [string[]]$Options = $subLevel.ForEach({($psitem -split "/")[$cLvel]}) | sort | Get-Unique #Extract all suboperation options
         $Options += "*"
         if ($Options.Count -gt 1) {
             if ($RootOP -match "(\/\*){2,}$") { #If last op is /*/* ask if done
@@ -38,16 +39,16 @@ param (
                  return New-Object System.Management.Automation.Host.ChoiceDescription "$psitem", "$psitem"
             })
             $result = $host.ui.PromptForChoice("Select operation", "Select", $optionsObject, 0)
-            if ($result + 1 -eq $Options.Count -and ($RootOP -match "(\/\*){2,}$")) {
+            if ($result + 1 -eq $Options.Count -and ($RootOP -match "(\/\*){2,}$")) { #If last option is selected and done is added exit.
                 $End=$true
             } else {
-                $RootOP = ("{0}/{1}" -f $RootOP,$optionsObject[$result].Label)
-                $cLvel++
+                $RootOP = ("{0}/{1}" -f $RootOP,$optionsObject[$result].Label) #Add selected suboption to new rootOperation
+                $cLvel++ #Increment deph
             }
         }
     }
     until (($End -eq $true) -or ($cLvel -eq $maxLevel) -or ($maxLevel -eq 0) -or ($Options.Count -le 1))
-    Write-Host $RootOP
+    Write-Verbose $RootOP
     if ((get-subOps -RootOP $RootOP).count -eq 0) { #Test OP
         throw "error"
     }
@@ -55,7 +56,7 @@ param (
     if ((get-subOps -RootOP $RootOP).count -eq 0) { #Test OP
         throw "error"
     }
-    if ($type -eq "Actions") {
+    if ($type -eq "Actions") { #Add to role Action / NotActions
         $Role.Actions.Add($RootOP)
     } else {
         $role.NotActions.Add($RootOP)
@@ -64,15 +65,15 @@ param (
 
 function Interactive-a365RBAC {
     $Role = Get-AzureRmRoleDefinition -Name "Reader" #Load Template
-    $Role.Id = ([guid]::NewGuid()).guid
-    $Role.IsCustom = $true
+    $Role.Id = ([guid]::NewGuid()).guid #Creat New random Guid
+    $Role.IsCustom = $true #Mark as custom
     $Role.Name = Read-Host -Prompt "Role Name" #Change to read-host
     $Role.Description = Read-Host -Prompt "Role Description" #Change to read-host
     $Role.Actions.Clear() #Remove all Actions
     $Role.NotActions.Clear() #Remove all Actions
     do
     {
-        AddProviderOperation -Role $Role -type Actions
+        AddProviderOperation -Role $Role -type Actions #Add Action
         [System.Management.Automation.Host.ChoiceDescription[]]$yn = New-Object System.Management.Automation.Host.ChoiceDescription "No", "No"
         $yn += New-Object System.Management.Automation.Host.ChoiceDescription "Yes", "Yes"
         $result = $host.ui.PromptForChoice("Add one more?", "", $yn, 0) 
@@ -86,7 +87,7 @@ function Interactive-a365RBAC {
     if ($result -eq 1) {
         do
         {
-            AddProviderOperation -Role $Role -type NotActions
+            AddProviderOperation -Role $Role -type NotActions #Add NotAction
             [System.Management.Automation.Host.ChoiceDescription[]]$yn = New-Object System.Management.Automation.Host.ChoiceDescription "No", "No"
             $yn += New-Object System.Management.Automation.Host.ChoiceDescription "Yes", "Yes"
             $result = $host.ui.PromptForChoice("Add one more?", "", $yn, 0) 
@@ -95,7 +96,6 @@ function Interactive-a365RBAC {
     }
 
     #Change scope.
-    #ToDo
     $Role.AssignableScopes.Clear() #Clear Scope
 
     #Ask for Global, Subscription or ResourceGroup scope
@@ -104,18 +104,15 @@ function Interactive-a365RBAC {
     $scope += New-Object System.Management.Automation.Host.ChoiceDescription "ResourceGroup", "Pick ResourceGroups"
     $ScopeLevel = $host.ui.PromptForChoice("Assignable Scopes?", "", $scope, 0)
 
-    if ($ScopeLevel -eq 0) {
-        #Global
+    if ($ScopeLevel -eq 0) { #Global
         $Role.AssignableScopes.Add("/")
-    } elseif ($ScopeLevel -eq 1) {
-        #Subscriptions
+    } elseif ($ScopeLevel -eq 1) { #Subscriptions
         [object[]]$Subscriptions = Get-AzureRmSubscription | ogv -OutputMode Multiple
         $Subscriptions.ForEach({
             $Sub = $PSItem.SubscriptionId
             $Role.AssignableScopes.Add(("/subscriptions/{0}" -f $Sub))
         })
-    } elseif ($ScopeLevel -eq 2) {
-        #ResourceGroup
+    } elseif ($ScopeLevel -eq 2) { #ResourceGroup
         [object[]]$Groups=Get-AzureRmResourceGroup | ogv -OutputMode Multiple
         $Groups.ForEach({
             [string]$Group=$psitem.ResourceId
@@ -125,10 +122,10 @@ function Interactive-a365RBAC {
     return $role
 }
 
-$newRole = Interactive-a365RBAC
-New-AzureRmRoleDefinition -Role $newRole
+$newRoleDef = Interactive-a365RBAC
+New-AzureRmRoleDefinition -Role $newRoleDef
 
-
+#Remove def
 break
 get-AzureRmRoleDefinition | ? {$_.IsCustom -eq $true} #Get custom
 Remove-AzureRmRoleDefinition -Name $newRole.Name #Remove role
