3fd1bc338ec8623d2639f5add46e19f1fcdd4204
jon.jander@altitude365.com <jon.jander@altitude365.com>
2016-12-15 11:59:04 +0100

    Added log display

---
+++ Tools/Azure SQL AuditLog Display/SQLDisplayLogs.ps1
@@ -0,0 +1,36 @@
+﻿$StorageAccountName = "StorageAccountName"
+$StorageAccountKey = "StorageAccountKey"
+$Tablename = "StorageTableName"
+[string[]]$dates = @(
+"20161115"
+)
+
+#Create Storage Account Context
+$Ctx = New-AzureStorageContext $StorageAccountName -StorageAccountKey $StorageAccountKey
+
+#Get all logs from all dates.
+$logObject = $dates.ForEach({
+    $tabName = ("{0}{1}" -f $Tablename,$PSItem)
+    try {
+    $tableclient = Get-AzureStorageTable $tabName -Context $Ctx -ErrorAction Stop
+    $query = New-Object Microsoft.WindowsAzure.Storage.Table.TableQuery
+    [object[]]$entities = $tableclient.CloudTable.ExecuteQuery($query)
+    [string[]]$Keys = $entities.Properties.Keys | sort | Get-Unique
+    [object[]]$retrunObject=$entities.ForEach({
+        [object[]]$item = $PSItem
+
+        $obj = New-Object System.Object
+        $Keys.ForEach({
+            $obj | Add-Member -MemberType NoteProperty -Name $PSItem -Value ($item.Properties[$PSItem].PropertyAsObject.ToString())
+        })
+        $obj 
+    })
+    return $retrunObject
+    } catch {}
+})
+
+#Get all ClientIP by log count
+$logObject | group -Property ClientIP | sort -Descending Count | select Count, Name
+
+#Out grid all logs
+$logObject | ogv
